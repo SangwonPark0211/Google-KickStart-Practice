6410f57107ec5ae44cd948cb4c942572bf1ee9f1
PSW <tkddnjs0211@gmail.com>
2021-10-08 15:01:48 +0900

    add TrashBins.py

--- 2021/Round F/TrashBins.py
+++ 2021/Round F/TrashBins.py
@@ -1,25 +1,30 @@
 T = int(input())
 for t in range(T):
     n = int(input())
-    arr = list(map(int, list(input())))
-    trash_house = []
-    if arr[0] != 1:
-        trash_house.append(-100)
-    for i in range(len(arr)):
-        if arr[i]==1:
-            trash_house.append(i)
-    if trash_house[-1] != n-1:
-        trash_house.append(n+100)
-
-    k=0
+    arr = input()
+    L, R = [500001 for _ in range(n)], [500001 for _ in range(n)]
+    # select left only
+    pos = -1
+    for i in range(n):
+        if arr[i]=='0' and pos==-1:
+            continue
+        if arr[i]=='1':
+            L[i] = 0
+            pos = i
+        else:
+            L[i] = i-pos
+    # select right only
+    pos = -1
+    for i in range(n-1,-1,-1):
+        if arr[i]=='0' and pos == -1:
+            continue
+        if arr[i]=='1':
+            R[i] = 0
+            pos = i
+        else:
+            R[i] = pos-i
     ans = 0
     for i in range(n):
-        if arr[i]==1:
-            k+=1
-            if trash_house[k]==n+100:
-                break
-            continue
-        ans += min(i-trash_house[k], trash_house[k+1]-i)
-    for i in range(trash_house[k-1]+1, n):
-        ans += arr[i] - trash_house[k-1]
+        ans += min(L[i], R[i])
+    
     print(f"Case #{t+1}: {ans}")
