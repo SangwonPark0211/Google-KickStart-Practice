e7cf4cac237e29cbfac880bcafb4417517c7b1ae
PSW <tkddnjs0211@gmail.com>
2021-10-10 15:44:19 +0900

    add Festival.py

---
+++ 2021/Round F/Festival.py
@@ -0,0 +1,24 @@
+T = int(input())
+for t in range(T):
+    D, N, K = list(map(int, input().split()))
+    attraction = []
+    for _ in range(N):
+        temp = list(map(int, input().split()))  # input rating, start day, end day
+        attraction.append(temp)
+    # sort attraction by rating in decreasing order
+    attraction.sort(key=lambda x:x[0], reverse=True)
+    day_arr = [[] for _ in range(3 * 10**5 + 1)]
+    for i in range(N):
+        r, s, e = attraction[i]
+        for j in range(s,e+1):
+            if len(day_arr[j])==K:
+                continue
+            day_arr[j].append(r)
+            
+    ans = -1
+    for i in range(len(day_arr)):
+        if day_arr[i]:
+            ans = max(ans, sum(day_arr[i][:K]))
+    print(f"Case #{t+1}: {ans}")
+
+
