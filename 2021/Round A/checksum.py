007bb79989b1e75ff57829a28ab999007abf4332
PSW <tkddnjs0211@gmail.com>
2022-01-16 15:57:29 +0900

    initial commit

--- 2021/Round A/checksum.py
+++ 2021/Round A/checksum.py
@@ -1,5 +1,5 @@
 # https://codingcompetitions.withgoogle.com/kickstart/round/0000000000436140/000000000068c2c3
-from collections import Counter
+
 from collections import deque
 
 def init():
@@ -35,8 +35,7 @@ def checksum():
         if A[row][col] != -1:
             continue
         A_row, A_col = row_col_extract(A, row, col)
-        counter_1, counter_2 = Counter(A_row), Counter(A_col)
-        if counter_1[-1] >= 2 and counter_2[-1] >= 2:
+        if A_row.count(-1) >= 2 and A_col.count(-1) >= 2:
             A[row][col] = 0 # restore
             ans += B[row][col]
             # print(f"restore ({row},{col})")
@@ -46,13 +45,16 @@ def checksum():
             while q:
                 r, c = q.popleft()
                 row_list, col_list = row_col_extract(A, r, c)
-                row_list_count, col_list_count = Counter(row_list), Counter(col_list)
-                if row_list_count[-1] == 1:
+                if row_list.count(-1) == 1:
                     q.append((r,row_list.index(-1)))
                     A[r][row_list.index(-1)] = 0
-                if col_list_count[-1] == 1:
+                if col_list.count(-1) == 1:
                     q.append((col_list.index(-1), c))
                     A[col_list.index(-1)][c] = 0
+        else:
+            A[row][col] = 0
+    # for i in range(N):
+    #     print(A[i])
     return ans
 
 for t in range(int(input())):
