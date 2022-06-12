3a38a61d4ce4119308989d5f5dce6ee892a975e1
PSW <tkddnjs0211@gmail.com>
2022-06-12 15:53:06 +0900

    initial commit

---
+++ 2020/Round G/combination_lock.py
@@ -0,0 +1,17 @@
+# https://codingcompetitions.withgoogle.com/kickstart/round/00000000001a0069/0000000000414a24
+import sys
+input = sys.stdin.readline
+
+def sol():
+    W, N = map(int, input().split())
+    arr = list(map(int, input().strip().split()))
+    ans = float('inf')
+    for target in arr:
+        temp_ans = 0
+        for i in range(len(arr)):
+            temp_ans += min(abs(target-arr[i]), N-abs(target-arr[i]))
+        ans = min(ans, temp_ans)
+    return ans
+    
+for case in range(int(input())):
+    print(f"Case #{case+1}: "+str(sol()))
