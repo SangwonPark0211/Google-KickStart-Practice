32e7e9219619217eac0413202efa23e0e334c110
PSW <tkddnjs0211@gmail.com>
2022-06-13 22:14:04 +0900

    edit : referred answer

--- 2020/Round G/combination_lock.py
+++ 2020/Round G/combination_lock.py
@@ -1,24 +1,19 @@
 # https://codingcompetitions.withgoogle.com/kickstart/round/00000000001a0069/0000000000414a24
-import sys
-input = sys.stdin.readline
+def combination_lock():
+    W, N = map(int, input().strip().split())
+    X = list(map(lambda x:int(x)-1, input().strip().split()))
+    X.sort()
+    for i in range(len(X)-1):  # make it as circular array
+        X.append(X[i]+N)
+    print(X)
+    prefix = [0]
+    for x in X:
+        prefix.append(prefix[-1]+x)
+    print(prefix)
+    temp = ((prefix[(i+W-1)+1]-prefix[(i+(i+W-1)+1)//2])-(prefix[(i+(i+W-1))//2+1]-prefix[i]) for i in range(W))
+    temp = list(temp)
+    print(temp)
+    return min((prefix[(i+W-1)+1]-prefix[(i+(i+W-1)+1)//2])-(prefix[(i+(i+W-1))//2+1]-prefix[i]) for i in range(W))  # find median of window with min number of moves
 
-def sol():
-    W, N = map(int, input().split())
-    arr = list(map(int, input().strip().split()))
-    arr.sort()
-    ans = float('inf')
-    target = arr[0]
-    temp_ans = 0
-    for i in range(len(arr)):
-        temp_ans += min(abs(target-arr[i]), N-abs(target-arr[i]))
-    ans = min(ans, temp_ans)
-    target = arr[-1]
-    temp_ans = 0
-    for i in range(len(arr)):
-        temp_ans += min(abs(target-arr[i]), N-abs(target-arr[i]))
-    ans = min(ans, temp_ans)
-    
-    return ans
-    
 for case in range(int(input())):
-    print(f"Case #{case+1}: "+str(sol()))
+    print('Case #%d: %s' % (case+1, combination_lock()))
