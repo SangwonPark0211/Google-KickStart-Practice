8256c57760d484d8ffa28fc9241f5e9805bad54d
PSW <tkddnjs0211@gmail.com>
2022-07-13 13:45:14 +0900

    initial commit

--- 2022/RoundD/B.py
+++ 2022/RoundD/B.py
@@ -1,35 +1,38 @@
 import sys
 input = sys.stdin.readline
 
+def getMaxSum(cnt, ltr, rtl):
+    case = [[i,cnt-i] for i in range(cnt+1)]
+    ans = 0
+    for f, b in case:
+        temp = ltr[f] + rtl[len(ltr)-1-b]
+        ans = max(temp, ans)
+    return ans
+    
 for t in range(int(input())):
     N = int(input())
-    arr1 = list(map(int, input().strip().split()))
+    A = list(map(int, input().strip().split()))
     M = int(input())
-    arr2 = list(map(int, input().strip().split()))
+    B = list(map(int, input().strip().split()))
     K = int(input())
 
-    dp1, dp2, dp3, dp4 = [0]*(N+1), [0]*(N+1), [0]*(M+1), [0]*(M+1)
-    for i in range(1, N+1):
-        dp1[i] = dp1[i-1] + arr1[i-1]
-    for i in range(N-1, -1, -1):
-        dp2[i] = dp2[i+1] + arr1[i]
-    for i in range(1, M+1):
-        dp3[i] = dp3[i-1] + arr2[i-1]
-    for i in range(M-1, -1, -1):
-        dp4[i] = dp4[i+1] + arr2[i]
+    A_ltr, A_rtl = [0]*(len(A)+1), [0]*(len(A)+1)
+    B_ltr, B_rtl = [0]*(len(B)+1), [0]*(len(B)+1)
+    for i in range(1, len(A_ltr)):
+        A_ltr[i] = A_ltr[i-1] + A[i-1]
+    for i in range(1, len(B_ltr)):
+        B_ltr[i] = B_ltr[i-1] + B[i-1]
+    for i in range(len(A_rtl)-2, -1, -1):
+        A_rtl[i] = A_rtl[i+1] + A[i]
+    for i in range(len(B_rtl)-2, -1, -1):
+        B_rtl[i] = B_rtl[i+1] + B[i]
+        
+    case = [[i,K-i] for i in range(K+1)]
     ans = 0
-    for x in range(N+1):
-        for y in range(N, -1, -1):
-            if x+(N-y) > K or x+(N-y)>N:
-                break
-            for w in range(M+1):
-                if x+(N-y)+w > K:
-                    break
-                for z in range(M, -1, -1):
-                    if x+(N-y)+w+(M-z) > K or w+(M-z)>M:
-                        break
-                    if x+(N-y)+w+(M-z) == K:
-                        # if x+(N-y)<=N and w+(M-z)<=M:
-                        ans = max(ans, dp1[x]+dp2[y]+dp3[w]+dp4[z])
-                        
+    for f,b in case:
+        if f>len(A) or b>len(B):
+            continue
+        else:
+            ans = max(ans, getMaxSum(f, A_ltr, A_rtl)+getMaxSum(b, B_ltr, B_rtl))
+
     print(f"Case #{t+1}: "+str(ans))
